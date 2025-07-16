fc103145940f81ee783928c661ec57a41c3f4d2b
SystemsPurge <naktiyoussef@proton.me>
2025-07-16 16:36:22 +0200

    add readme, requirements.txt , fix env source scripts
    Signed-off-by: SystemsPurge <naktiyoussef@proton.me>

--- scripts/client-env.sh
+++ scripts/client-env.sh
@@ -1,7 +1,7 @@
 export DPS_MODE=client
 export DPS_ADDR=http://localhost:5000
-export DPS_DEFAULTS=/home/yna/trash/dps/defaults.json
+export DPS_DEFAULTS=<path to defaults file>
 export DPS_LOG_LEVEL=INFO
 dps(){
-    python3 /home/yna/trash/dps/src/script.py "$@"
+    python3 <path to cli-script.py> "$@"
 }
---
+++ scripts/sv-env.sh
@@ -0,0 +1,4 @@
+export DPS_ROOT=<path to file db workdir>
+export DPS_EXPORT=true
+export DPS_MODE=local
+export DPS_LOG_LEVEL=INFO
