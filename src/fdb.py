63551d47ac92a22d12600d00dc04d0b6780298fd
SystemsPurge <naktiyoussef@proton.me>
2025-11-25 15:31:18 +0100

    fix double extension fdb workaround bug
    Signed-off-by: SystemsPurge <naktiyoussef@proton.me>

--- src/fdb.py
+++ src/fdb.py
@@ -62,7 +62,7 @@ class fdb:
         self.__check(tstype)
         with open(self.__path(fdb.__ts[tstype],tsname),'bw') as f:
             f.write(content)
-        df = self.tsget(tstype,tsname) #retrieve as df
+        df = self.tsget(tstype,tsname.split('.')[0]) #retrieve as df
         for c in df.columns:
             df.rename({c:c.strip()},inplace=True,axis=1)
         self.tsput(tstype,tsname,df)
